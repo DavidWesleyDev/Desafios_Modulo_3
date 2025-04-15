5bf5e8a0b04fcd05701c9edf22a0f4bdddc7a7c0
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-14 22:57:40 -0300

    Contagem Progressiva.
    Desafio 4 - Curso de logica de programação, desafio de 0 ao numero escolhido pelo usuário

---
+++ desafio_4/app.js
@@ -0,0 +1,8 @@
+let contadorMaximo = Number(prompt('Digite um número:'))
+let contador = 0;
+
+while (contador <= contadorMaximo){
+    console.log(contador);
+    alert(contador);
+    contador++;
+};
