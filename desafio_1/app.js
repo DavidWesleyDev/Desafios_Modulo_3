220dad8cc59a5fedf96e1568c6c26ac2f2a42e8b
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-14 21:38:52 -0300

    contagem de 1 a 10
    Finalização do desafio do curso de logica de programação, que faz a contagem de 1 a 10 com o while

---
+++ desafio_1/app.js
@@ -0,0 +1,14 @@
+// Inicializa a variável 'contador' com o valor 1
+let contador = 1;
+
+// Início do laço 'while' que continuará enquanto 'contador' for menor ou igual a 10
+while(contador <= 10){
+    // Exibe o valor atual de 'contador' no console do navegador
+    console.log(contador);
+
+    // Exibe um alerta na tela com o valor atual de 'contador'
+    alert(contador);
+
+    // Incrementa o valor de 'contador' em 1
+    contador++;
+}; // Fim do laço
